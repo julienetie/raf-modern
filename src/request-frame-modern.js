f5987d88b67a61a92b73bd8051b39ab4cf51339a
Julien Etienne <j8@protonmail.ch>
2017-04-03 16:56:52 +0100

    Updated comments

--- src/request-frame-modern.js
+++ src/request-frame-modern.js
@@ -3,17 +3,17 @@ var previousTime = 0;
 
 
 /**
- * Native clearTimeout function.
+ * Native clearTimeout function for IE-9 cancelAnimationFrame
  * @return {Function}
  */
-function clearTimeoutWithId(id) {
+const clearTimeoutWithId = (id) => {
     window.clearTimeout(id);
     id = null;
 }
 
 
 /**
- * 
+ * IE-9 Polyfill for requestAnimationFrame
  * @callback {Number} Timestamp.
  * @return {Function} setTimeout Function.
  */
@@ -26,37 +26,41 @@ function setTimeoutWithTimestamp(callback) {
         lapsedTime - immediateTime);
 }
 
+
 // Request and cancel functions for IE9+ & modern mobile browsers. 
 const requestFrameFn = window.requestAnimationFrame || setTimeoutWithTimestamp;
 const cancelFrameFn = window.cancelAnimationFrame || clearTimeoutWithId;
 
 
 /**
- * 
+ * Set the requestAnimationFrame & cancelAnimationFrame window functions.
  */
-const setNativeFn = (winObj, requestFn, cancelFn) => {
+const setNativeFn = (requestFn, cancelFn, winObj) => {
     winObj.requestAnimationFrame = requestFn;
     winObj.cancelAnimationFrame = cancelFn;
 }
 
 
 /**
+ * Default function to set the timing.
  * @param  {String} type - request | cancel | native | ''.
  * @return {Function} Timing function.
  */
 const requestFrameModern = (type) => {
+    const errorMessage = 'RequestFrame parameter is not a type.';
+    const native = 'native';
     const timingType = {};
 
     timingType.request = requestFrameFn;
     timingType.cancel = cancelFrameFn
     timingType[''] = timingType.request;
 
-    if (type === 'native') {
-        return setNativeFn(window, requestFrameFn, cancelFrameFn);
+    if (type === native) {
+        return setNativeFn(requestFrameFn, cancelFrameFn, window);
     }
 
     if (!timingType.hasOwnProperty(type)) {
-        throw new Error('RequestFrame parameter is not a type.');
+        throw new Error(errorMessage);
     }
 
     return timingType[type];
